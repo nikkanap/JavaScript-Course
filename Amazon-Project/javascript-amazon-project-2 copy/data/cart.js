bd8a6d95af9adbfb006447a6bb571b98a7bac48b
nikkanap <nenaputo@up.edu.ph>
2025-09-16 20:53:18 +0800

    create automated tests for addToCart

--- Amazon-Project/javascript-amazon-project-2 copy/data/cart.js
+++ Amazon-Project/javascript-amazon-project-2 copy/data/cart.js
@@ -1,16 +1,6 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || 
-[
-    {
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2,
-        deliveryOptionId: '1'
-    },
-    {
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 1,
-        deliveryOptionId: '2'
-    }
-];
+export let cart;
+
+loadFromStorage();
 
 export function addToCart(productId) {
     let matchingItem;
@@ -20,7 +10,8 @@ export function addToCart(productId) {
         } 
     });
 
-    const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+    //const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+    const quantity = 1;
     if(matchingItem){
         matchingItem.quantity += quantity;
     } else {
@@ -44,6 +35,22 @@ export function removeFromCart(productId) {
 
     cart = newCart;
     saveToStorage();
+}
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart')) || 
+    [
+        {
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2,
+            deliveryOptionId: '1'
+        },
+        {
+            productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1,
+            deliveryOptionId: '2'
+        }
+    ];
 }
 
 function saveToStorage() {
